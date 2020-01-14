39054e8c552f90cca069d257bdeef1fd889a1d92
IrateLeaf98 <40268761+vaibhavsethia@users.noreply.github.com>
2020-01-14 11:52:53 +0530

    Update BoardingGate.js

--- src/Components/New Delhi/BoardingGate.js
+++ src/Components/New Delhi/BoardingGate.js
@@ -1,5 +1,6 @@
+/*eslint-disable */
 import React, { Component } from 'react'
-import {VictoryPie,VictoryTooltip} from 'victory'
+import {VictoryPie,VictoryTooltip,VictoryBar,VictoryAxis,VictoryChart,VictoryTheme} from 'victory'
 import './StylesN.css'
 
 export class BoardingGate extends Component {
@@ -14,19 +15,33 @@ export class BoardingGate extends Component {
             Gate4:9,
             Gate5:9,
             Gate6:7,
+            UtilGate1:70,
+            UtilGate2:55,
+            UtilGate3:60,
+            UtilGate4:30,
+            UtilGate5:50,
+            UtilGate6:20,
         }
     }
     
     render() {
-        const {Total,Gate1,Gate2,Gate3,Gate4,Gate5,Gate6}=this.state;
+        const {Total,Gate1,Gate2,Gate3,Gate4,Gate5,Gate6,UtilGate1,UtilGate2,UtilGate3,UtilGate4,UtilGate5,UtilGate6}=this.state;
         const data = [
-            {x: parseFloat(Gate1*100/Total).toFixed(2) , y: parseInt(Gate1),label:"Gate1"+"-"+Gate1+"M"},
-            {x: parseFloat(Gate2*100/Total).toFixed(2) , y: parseInt(Gate2),label:"Gate2"+"-"+Gate2+"M"},
-            {x: parseFloat(Gate3*100/Total).toFixed(2) , y: parseInt(Gate3),label:"Gate3"+"-"+Gate3+"M"},
-            {x: parseFloat(Gate4*100/Total).toFixed(2) , y: parseInt(Gate4),label:"Gate4"+"-"+Gate4+"M"},
-            {x: parseFloat(Gate5*100/Total).toFixed(2) , y: parseInt(Gate5),label:"Gate5"+"-"+Gate5+"M"},
-            {x: parseFloat(Gate6*100/Total).toFixed(2) , y: parseInt(Gate6),label:"Gate6"+"-"+Gate6+"M"},
+            {x: parseFloat(Gate1*100/Total).toFixed(2) , y: parseFloat(Gate1),label:"Gate1"+"-"+Gate1+"M"},
+            {x: parseFloat(Gate2*100/Total).toFixed(2) , y: parseFloat(Gate2),label:"Gate2"+"-"+Gate2+"M"},
+            {x: parseFloat(Gate3*100/Total).toFixed(2) , y: parseFloat(Gate3),label:"Gate3"+"-"+Gate3+"M"},
+            {x: parseFloat(Gate4*100/Total).toFixed(2) , y: parseFloat(Gate4),label:"Gate4"+"-"+Gate4+"M"},
+            {x: parseFloat(Gate5*100/Total).toFixed(2) , y: parseFloat(Gate5),label:"Gate5"+"-"+Gate5+"M"},
+            {x: parseFloat(Gate6*100/Total).toFixed(2) , y: parseFloat(Gate6),label:"Gate6"+"-"+Gate6+"M"},
           ];
+        const Utildata=[
+            {Utilization: parseFloat(UtilGate1*100/Total).toFixed(2) , Gate: parseInt(UtilGate1),label:UtilGate1+"%"},
+            {Utilization: parseFloat(UtilGate2*100/Total).toFixed(2) , Gate: parseInt(UtilGate2),label:UtilGate2+"%"},
+            {Utilization: parseFloat(UtilGate3*100/Total).toFixed(2) , Gate: parseInt(UtilGate3),label:UtilGate3+"%"},
+            {Utilization: parseFloat(UtilGate4*100/Total).toFixed(2) , Gate: parseInt(UtilGate4),label:UtilGate4+"%"},
+            {Utilization: parseFloat(UtilGate5*100/Total).toFixed(2) , Gate: parseInt(UtilGate5),label:UtilGate5+"%"},
+            {Utilization: parseFloat(UtilGate6*100/Total).toFixed(2) , Gate: parseInt(UtilGate6),label:UtilGate6+"%"}
+        ];
         return (
             <div>
                 <p className="f2 tc mb1">Indira Gandhi International Airport</p>
@@ -34,6 +49,7 @@ export class BoardingGate extends Component {
                 <p className="f3 pa2 tc mb1">
                     Comparison among the entry gates used at IGI Airport is as follows: 
                 </p>
+                <div>
                 <div className="fl w-60 pa3 f4">
                     <VictoryPie 
                         labelComponent={<VictoryTooltip
@@ -47,7 +63,7 @@ export class BoardingGate extends Component {
                                 return [
                                   {
                                     target: "data",
-                                    mutation: () => ({style: {fill: "gold", width: 30}})
+                                    mutation: () => ({style: {fill: "gray", width: 30}})
                                   }, {
                                     target: "labels",
                                     mutation: () => ({ active: true })
@@ -114,11 +130,66 @@ export class BoardingGate extends Component {
                             </div>
                             <div className="w-100 pa3 mr2 Key-hover">
                                 <div className="link black f4  " href="#" title="CSMI Airport" >
-                                    <span className="Home-key-box Home-key-box-six"></span>: Gate5
+                                    <span className="Home-key-box Home-key-box-six"></span>: Gate6
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                
+                    <div className="fl w-70 pl3 tc">
+                    <p className="f3 pa2 tc mb2 ">
+                    Percentage Utilization of each Boarding Gate at IGI Airport is as follows: 
+                    </p>
+                        <VictoryChart 
+                            domainPadding={20} 
+                            theme={VictoryTheme.material} 
+                            animate={{duration: 2000}}
+                            style={{ parent: { maxWidth: "100%" } }}>
+                                <VictoryAxis
+                                    tickValues={[1, 2, 3, 4,5,6,7,8,9,10]}
+                                    tickFormat={["B1", "B2", "B3", "B4","B5", "B6", "B7", "B8","B9", "B10"]}
+                                />
+                                <VictoryAxis
+                                    dependentAxis
+                                    tickFormat={(y) => (`$${y}%`)}
+                                />
+                                <VictoryBar
+                                    data={Utildata}
+                                    events={[{
+                                        target: "data",
+                                        eventHandlers: {
+                                          onMouseOver: () => {
+                                            return [
+                                              {
+                                                target: "data",
+                                                mutation: () => ({style: {fill: "gray", width: 30}})
+                                              }, {
+                                                target: "labels",
+                                                mutation: () => ({ active: true })
+                                              }
+                                            ];
+                                          },
+                                          onMouseOut: () => {
+                                            return [
+                                              {
+                                                target: "data",
+                                                mutation: () => {}
+                                              }, {
+                                                target: "labels",
+                                                mutation: () => ({ active: false })
+                                              }
+                                            ];
+                                          }
+                                        }
+                                      }]}
+            
+                                    barWidth={({ index }) => 5}
+                                    x="Utilization"
+                                    y="Gate"
+                                />
+                            </VictoryChart>
+                        </div>
             </div>
         )
     }
